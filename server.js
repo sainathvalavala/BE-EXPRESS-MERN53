12c2e30101d1404dbf3eb4dd54b821984214f7c5
Sainathgit config --global user.name Sainathgit config --global user.name Sainath <sainath.valavala.31@gmail.com>
2025-10-22 14:59:55 +0530

    added post request by fetch(on client side) and using app.use(bodyparser.json())

--- server.js
+++ server.js
@@ -1,10 +1,32 @@
 const express = require("express");
 const app = express();
-
+const cors = require("cors");
+const fs = require("fs");
+const bodyparser = require("body-parser");
+app.use(cors());
+app.use(bodyparser.urlencoded({ extended: false }));
+app.use(bodyparser.json())
+app.use(express.static(__dirname + "/public"));
 app.get("/", (req, res) => {
   // console.log(req);
-  console.log(res);
-  res.json({a:"hello"})
+  // console.log(res);
+  res.json({ a: "hello" });
+});
+
+app.post("/postTodo", (req, res) => {
+  // console.log(req.body);
+  var f1 = JSON.parse(fs.readFileSync("file.txt"));
+  f1.push(req.body);
+  fs.writeFileSync("file.txt", JSON.stringify(f1));
+  console.log(f1);
+  // var f2= fs.readFileSync("file.txt")
+
+  res.json(f1);
+});
+app.get("/allTodos", (req, res) => {
+  var f1 = fs.readFileSync("file.txt").toString();
+  res.json(JSON.parse(f1));
+  
 });
 
 app.listen(4000, () => {
