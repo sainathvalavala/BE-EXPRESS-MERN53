8a76b0b873dc06a6e00449fab68ab9d0f97df58a
Sainathgit config --global user.name Sainathgit config --global user.name Sainath <sainath.valavala.31@gmail.com>
2025-10-23 18:09:42 +0530

    added middlewares

--- server.js
+++ server.js
@@ -7,16 +7,37 @@ app.use(cors());
 app.use(bodyparser.urlencoded({ extended: false }));
 app.use(bodyparser.json());
 app.use(express.static(__dirname + "/public"));
+
+const checklist=[authenticate,authorize1]
 //  /=>endpoint, route
 //endpoint => data ni istey?
 //route => page ni iste?
 
+//middleware--
+
+// app.use(function (req,res,next){
+//   console.log("General Middleware called");
+//   next()
+// })
+function middleware(req, res, next) {
+  console.log("General Middleware called");
+  next();
+}
+app.use(middleware);
+
 //request handling
-app.get("/", (req, res) => {
-  // console.log(req);
-  // console.log(res);
-  res.json({ a: "hello" });
-});
+app.get(
+  "/",
+  function (req, res, next) {
+    console.log("Default Middleware called");
+    next();
+  },
+  (req, res) => {
+    // console.log(req);
+    // console.log(res);
+    res.json({ a: "hello" });
+  }
+);
 
 app.post("/postTodo", (req, res) => {
   // console.log(req.body);
@@ -29,12 +50,39 @@ app.post("/postTodo", (req, res) => {
   res.json(f1);
 });
 
-app.get("/allTodos", (req, res) => {
-  var f1 = fs.readFileSync("file.txt").toString();
-  res.json(JSON.parse(f1));
+app.get(
+  "/allTodos",
+  function (req, res, next) {
+    console.log("displayed all todos");
+    next();
+  },
+  (req, res) => {
+    var f1 = fs.readFileSync("file.txt").toString();
+    res.json(JSON.parse(f1));
+  }
+);
+
+//if middleware is here, above endpoints are responded  without middleware function.
+
+// app.use(function (req,res,next){
+//   console.log("General Middleware called");
+//   next()
+// })
+function authenticate(req, res, next) {
+  console.log("Authentication jarigindhi");
+  next();
+}
+
+function authorize1(req, res, next) {
+  console.log("authorize1");
+  next();
+}
+
+app.get("/getMovieById/:m1",checklist, (req, res) => {
+  res.send("idigo thesuko");
 });
 
-app.get("/getTodosByUserName/:username", (req, res) => {
+app.get("/getTodosByUserName/:username", middleware, (req, res) => {
   var f1 = JSON.parse(fs.readFileSync("file.txt").toString());
   var filteredData = f1.filter(function (todo) {
     if (todo.username === req.params.username) {
